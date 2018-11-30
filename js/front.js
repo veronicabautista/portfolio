01bfd1937466bfaab1c37282136ec23a4493699d
Veronica Bautista <veronicabautista87@gmail.com>
2018-11-30 22:00:26 +0100

    top button

--- js/front.js
+++ js/front.js
@@ -1,19 +1,44 @@
-$(function () {
-  $('#filter a').click(function (e) {
+$(function() {
+
+		// -- Projects menu -- //
+
+  $("#filter a").click(function(e) {
     e.preventDefault();
 
-    $('#filter li').removeClass('active');
-    $(this).parent('li').addClass('active');
+    $("#filter li").removeClass("active");
+    $(this)
+      .parent("li")
+      .addClass("active");
 
-    var categoryToFilter = $(this).attr('data-filter');
+    var categoryToFilter = $(this).attr("data-filter");
 
-    $('.reference-item').each(function () {
-
-        if ($(this).data('category') === categoryToFilter || categoryToFilter === 'all') {
-            $(this).show();
-        } else {
-            $(this).hide();
-        }
+    $(".reference-item").each(function() {
+      if (
+        $(this).data("category") === categoryToFilter ||
+        categoryToFilter === "all"
+      ) {
+        $(this).show();
+      } else {
+        $(this).hide();
+      }
     });
   });
+
+	
+	// -- Go top button -- //
+
+	$('.go-top').click(function(){
+		$('body, html').animate({
+				scrollTop: '0px'
+		}, 1000);
+	});
+
+	$(window).scroll(function(){
+			if( $(this).scrollTop() > 0 ){
+					$('.go-top').slideDown(800);
+			} else {
+					$('.go-top').slideUp(800);
+			}
+	});
+
 });
